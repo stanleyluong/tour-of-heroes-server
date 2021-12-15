59243c62914562c23a0af78dc3c693f7777b2ef0
Stanley Luong <slluong@uw.edu>
2021-12-14 23:58:11 -0800

    added cors

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200; // <== You can change the port
+const cors = require('cors')
 
 server.use(middlewares);
 server.use(router);
-
+server.use(cors)
 server.listen(port);
