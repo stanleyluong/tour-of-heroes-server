973b248cdae530374fee35d792eb91f81d4e9532
Stanley Luong <slluong@uw.edu>
2021-12-15 00:12:26 -0800

    changed noCors to true

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // <== Will be created later
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({noCors: true});
 const port = process.env.PORT || 3200; // <== You can change the port
-const cors = require('cors')
+// const cors = require('cors')
 
 server.use(middlewares);
 server.use(router);
-server.use(cors)
+// server.use(cors)
 server.listen(port);
